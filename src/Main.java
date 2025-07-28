3d09ebd88dead2ab51684eeac3148831d96bee1c
Erhan <erbredell@gmail.com>
2025-07-28 13:53:21 +0200

    Math expr
    Check for even number of brackets done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import math.base.Math_Reader;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Math_Reader.convertExpression("(()");
 	}
 
 }
--- src/math/base/Math_Reader.java
+++ src/math/base/Math_Reader.java
@@ -8,5 +8,22 @@ package math.base;
 public class Math_Reader {
 	private String expression;
 	
-	
+	public static void convertExpression(String expression) {
+		
+		String[] split = expression.split("(?!^)"); //Split every char of the given sequence
+		int bracketCounter = 0;
+		for(int i = 0; i < split.length; i++) {
+			System.out.println(split[i]);
+			if(split[i].equals("(") || split[i].equals(")") || split[i].equals("[")|| split[i].equals("]")) {
+				bracketCounter++;
+				System.out.println("Bracket hit: " + split[i]);
+			}
+			System.out.println(bracketCounter);
+		}
+		if((bracketCounter % 2) != 0) {
+			System.err.println("Unequal bracket amount present, please fill in missing");
+			return;
+		}
+		System.out.println("Test");
+	}
 }
